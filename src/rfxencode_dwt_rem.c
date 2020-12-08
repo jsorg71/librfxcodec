4cb7e227fd960804f42b92c33c3adf058308ae82
Jay Sorg <jay.sorg@gmail.com>
2020-12-07 23:06:24 -0800

    optimization for dwt rem

--- src/rfxencode_dwt_rem.c
+++ src/rfxencode_dwt_rem.c
@@ -29,6 +29,17 @@
 #include "rfxcommon.h"
 #include "rfxencode_dwt_rem.h"
 
+#define ICL1(_offset) (ic[(_offset) * 64] - 128) << DWT_FACTOR
+#define ICL2(_offset) ic[(_offset) * 33]
+#define ICL3(_offset) ic[(_offset) * 17]
+
+#define HIL1(_offset) hptr[(_offset) * 64]
+#define LOL1(_offset) lptr[(_offset) * 64]
+#define HIL2(_offset) hptr[(_offset) * 33]
+#define LOL2(_offset) lptr[(_offset) * 33]
+#define HIL3(_offset) hptr[(_offset) * 17]
+#define LOL3(_offset) lptr[(_offset) * 17]
+
 /******************************************************************************/
 static void
 rfx_rem_dwt_2d_encode_vert_lv1(const uint8 *in_buffer, sint16 *out_buffer)
@@ -40,62 +51,49 @@ rfx_rem_dwt_2d_encode_vert_lv1(const uint8 *in_buffer, sint16 *out_buffer)
     sint16 x2n2;    /* n[2n + 2] */
     sint16 hn1;     /* H[n - 1]  */
     sint16 hn;      /* H[n]      */
-    const uint8 *src;
     int n;
     int y;
-    sint16 ic[68];
+    const uint8 *ic;
+    sint16 ic62;
 
     for (y = 0; y < 64; y++)
     {
 
         /* setup */
-        src = in_buffer + y;
-        for (n = 0; n < 64; n++)
-        {
-            ic[y] = (src[n * 64] - 128) << DWT_FACTOR;
-        }
-        ic[64] = 2 * ic[63] - ic[62];
-        ic[65] = ic[63];
-        ic[66] = ic[62];
+        ic = in_buffer + y;
 
         /* pre */
         lptr = out_buffer + y;
         hptr = lptr + 64 * 33;
-        x2n = ic[0];
-        x2n1 = ic[1];
-        x2n2 = ic[2];
-        *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + hn;
+        x2n = ICL1(0);
+        x2n1 = ICL1(1);
+        x2n2 = ICL1(2);
+        HIL1(0) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+        LOL1(0) = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 31; n++)
         {
             hn1 = hn;
-            lptr += 64;
-            hptr += 64;
-            x2n = ic[2 * n];
-            x2n1 = ic[2 * n + 1];
-            x2n2 = ic[2 * n + 2];
-            *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-            *lptr = x2n + ((hn1 + hn) >> 1);
+            x2n = x2n2;
+            x2n1 = ICL1(2 * n + 1);
+            x2n2 = ICL1(2 * n + 2);
+            HIL1(n) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+            LOL1(n) = x2n + ((hn1 + hn) >> 1);
         }
 
         /* post */
         hn1 = hn;
-        lptr += 64;
-        x2n = ic[2 * 31];
-        x2n1 = ic[2 * 31 + 1];
-        x2n2 = ic[2 * 31 + 2];
-        hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        ic62 = x2n = x2n2;
+        x2n1 = ICL1(63);
+        x2n2 = 2 * x2n1 - x2n; /* ic[64] = 2 * ic[63] - ic[62] */
+        LOL1(31) = x2n + (hn1 >> 1);
 
-        hn1 = hn;
-        lptr += 64;
-        x2n = ic[2 * 32];
-        x2n1 = ic[2 * 32 + 1];
-        x2n2 = ic[2 * 32 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 65 -> 63 */
+        x2n2 = ic62;      /* mirror 66 -> 62 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        LOL1(32) = x2n + (hn >> 1);
 
     }
 }
@@ -113,42 +111,34 @@ rfx_rem_dwt_2d_encode_horz_lv1(const sint16 *in_buffer, sint16 *out_buffer)
     sint16 x2n2;    /* n[2n + 2] */
     sint16 hn1;     /* H[n - 1]  */
     sint16 hn;      /* H[n]      */
-    const sint16 *l_src;
-    const sint16 *h_src;
     int n;
     int y;
-    sint16 ic[68];
+    const sint16 *ic;
+    sint16 ic62;
 
     hl = out_buffer;
     lh = hl + 31 * 33;
     hh = lh + 33 * 31;
     ll = hh + 31 * 31;
 
-    for (y = 0; y < 33; y++)
+    for (y = 0; y < 33; y++) /* low */
     {
 
         /* setup */
-        l_src = in_buffer + y * 64;
-        for (n = 0; n < 64; n++)
-        {
-            ic[n] = l_src[n];
-        }
-        ic[64] = 2 * ic[63] - ic[62];
-        ic[65] = ic[63];
-        ic[66] = ic[62];
+        ic = in_buffer + 64 * y;
 
         /* pre */
         x2n = ic[0];
         x2n1 = ic[1];
         x2n2 = ic[2];
         hl[0] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        ll[0] = x2n + hn;
+        ll[0] = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 31; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hl[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -157,33 +147,24 @@ rfx_rem_dwt_2d_encode_horz_lv1(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 31];
-        x2n1 = ic[2 * 31 + 1];
-        x2n2 = ic[2 * 31 + 2];
-        hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        ll[31] = x2n + ((hn1 + hn) >> 1);
+        ic62 = x2n = x2n2;
+        x2n1 = ic[63];
+        x2n2 = 2 * x2n1 - x2n; /* ic[64] = 2 * ic[63] - ic[62] */
+        ll[31] = x2n + (hn1 >> 1);
 
-        hn1 = hn;
-        x2n = ic[2 * 32];
-        x2n1 = ic[2 * 32 + 1];
-        x2n2 = ic[2 * 32 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 65 -> 63 */
+        x2n2 = ic62;      /* mirror 66 -> 62 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        ll[32] = x2n + ((hn1 + hn) >> 1);
+        ll[32] = x2n + (hn >> 1);
 
     }
 
-    for (y = 0; y < 31; y++)
+    for (y = 0; y < 31; y++) /* high */
     {
 
         /* setup */
-        h_src = l_src + 64 * 33 + y * 64;
-        for (n = 0; n < 64; n++)
-        {
-            ic[n] = h_src[n];
-        }
-        ic[64] = 2 * ic[63] - ic[62];
-        ic[65] = ic[63];
-        ic[66] = ic[62];
+        ic = in_buffer + 64 * (33 + y);
 
         /* pre */
         x2n = ic[0];
@@ -196,7 +177,7 @@ rfx_rem_dwt_2d_encode_horz_lv1(const sint16 *in_buffer, sint16 *out_buffer)
         for (n = 1; n < 31; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hh[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -205,18 +186,16 @@ rfx_rem_dwt_2d_encode_horz_lv1(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 31];
-        x2n1 = ic[2 * 31 + 1];
-        x2n2 = ic[2 * 31 + 2];
-        hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        lh[31] = x2n + ((hn1 + hn) >> 1);
+        ic62 = x2n = x2n2;
+        x2n1 = ic[63];
+        x2n2 = 2 * x2n1 - x2n; /* ic[64] = 2 * ic[63] - ic[62] */
+        lh[31] = x2n + (hn1 >> 1);
 
-        hn1 = hn;
-        x2n = ic[2 * 32];
-        x2n1 = ic[2 * 32 + 1];
-        x2n2 = ic[2 * 32 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 65 -> 63 */
+        x2n2 = ic62;      /* mirror 66 -> 62 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        lh[32] = x2n + ((hn1 + hn) >> 1);
+        lh[32] = x2n + (hn >> 1);
 
     }
 
@@ -233,62 +212,51 @@ rfx_rem_dwt_2d_encode_vert_lv2(const sint16 *in_buffer, sint16 *out_buffer)
     sint16 x2n2;    /* n[2n + 2] */
     sint16 hn1;     /* H[n - 1]  */
     sint16 hn;      /* H[n]      */
-    const uint16 *src;
     int n;
     int y;
-    sint16 ic[36];
+    const sint16 *ic;
+    sint16 ic30;
 
     for (y = 0; y < 33; y++)
     {
 
         /* setup */
-        src = in_buffer + y;
-        for (n = 0; n < 33; n++)
-        {
-            ic[y] = src[n * 33];
-        }
-        ic[33] = ic[31];
-        ic[34] = ic[30];
+        ic = in_buffer + y;
 
         /* pre */
         lptr = out_buffer + y;
         hptr = lptr + 33 * 17;
-        x2n = ic[0];
-        x2n1 = ic[1];
-        x2n2 = ic[2];
-        *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + hn;
+        x2n = ICL2(0);
+        x2n1 = ICL2(1);
+        x2n2 = ICL2(2);
+        HIL2(0) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+        LOL2(0) = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 15; n++)
         {
             hn1 = hn;
-            lptr += 33;
-            hptr += 33;
-            x2n = ic[2 * n];
-            x2n1 = ic[2 * n + 1];
-            x2n2 = ic[2 * n + 2];
-            *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-            *lptr = x2n + ((hn1 + hn) >> 1);
+            x2n = x2n2;
+            x2n1 = ICL2(2 * n + 1);
+            x2n2 = ICL2(2 * n + 2);
+            HIL2(n) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+            LOL2(n) = x2n + ((hn1 + hn) >> 1);
         }
 
         /* post */
         hn1 = hn;
-        lptr += 33;
-        hptr += 33;
-        x2n = ic[2 * 15];
-        x2n1 = ic[2 * 15 + 1];
-        x2n2 = ic[2 * 15 + 2];
-        *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        ic30 = x2n = x2n2;
+        x2n1 = ICL2(31);
+        x2n2 = ICL2(32);
+        HIL2(15) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+        LOL2(15) = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        lptr += 33;
-        x2n = ic[2 * 16];
-        x2n1 = ic[2 * 16 + 1];
-        x2n2 = ic[2 * 16 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 33 -> 31 */
+        x2n2 = ic30;      /* mirror 34 -> 30 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        LOL2(16) = x2n + ((hn1 + hn) >> 1);
 
     }
 }
@@ -306,28 +274,21 @@ rfx_rem_dwt_2d_encode_horz_lv2(const sint16 *in_buffer, sint16 *out_buffer)
     sint16 x2n2;    /* n[2n + 2] */
     sint16 hn1;     /* H[n - 1]  */
     sint16 hn;      /* H[n]      */
-    const sint16 *l_src;
-    const sint16 *h_src;
     int n;
     int y;
-    sint16 ic[36];
+    const sint16 *ic;
+    sint16 ic30;
 
     hl = out_buffer;
     lh = hl + 16 * 17;
     hh = lh + 17 * 16;
     ll = hh + 16 * 16;
 
-    for (y = 0; y < 17; y++)
+    for (y = 0; y < 17; y++) /* low */
     {
 
         /* setup */
-        l_src = in_buffer + y * 33;
-        for (n = 0; n < 33; n++)
-        {
-            ic[n] = l_src[n];
-        }
-        ic[33] = ic[31];
-        ic[34] = ic[30];
+        ic = in_buffer + 33 * y;
 
         /* pre */
         x2n = ic[0];
@@ -340,7 +301,7 @@ rfx_rem_dwt_2d_encode_horz_lv2(const sint16 *in_buffer, sint16 *out_buffer)
         for (n = 1; n < 15; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hl[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -349,32 +310,26 @@ rfx_rem_dwt_2d_encode_horz_lv2(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 15];
-        x2n1 = ic[2 * 15 + 1];
-        x2n2 = ic[2 * 15 + 2];
+        ic30 = x2n = x2n2;
+        x2n1 = ic[31];
+        x2n2 = ic[32];
         hl[15] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         ll[15] = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        x2n = ic[2 * 16];
-        x2n1 = ic[2 * 16 + 1];
-        x2n2 = ic[2 * 16 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 33 -> 31 */
+        x2n2 = ic30;      /* mirror 34 -> 30 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         ll[16] = x2n + ((hn1 + hn) >> 1);
 
     }
 
-    for (y = 0; y < 16; y++)
+    for (y = 0; y < 16; y++) /* high */
     {
 
         /* setup */
-        h_src = l_src + 33 * 17 + y * 33;
-        for (n = 0; n < 33; n++)
-        {
-            ic[n] = h_src[n];
-        }
-        ic[33] = ic[31];
-        ic[34] = ic[30];
+        ic = in_buffer + 33 * (17 + y);
 
         /* pre */
         x2n = ic[0];
@@ -387,7 +342,7 @@ rfx_rem_dwt_2d_encode_horz_lv2(const sint16 *in_buffer, sint16 *out_buffer)
         for (n = 1; n < 15; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hh[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -396,16 +351,16 @@ rfx_rem_dwt_2d_encode_horz_lv2(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 15];
-        x2n1 = ic[2 * 15 + 1];
-        x2n2 = ic[2 * 15 + 2];
+        ic30 = x2n = x2n2;
+        x2n1 = ic[31];
+        x2n2 = ic[32];
         hh[15] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         lh[15] = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        x2n = ic[2 * 16];
-        x2n1 = ic[2 * 16 + 1];
-        x2n2 = ic[2 * 16 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 33 -> 31 */
+        x2n2 = ic30;      /* mirror 34 -> 30 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         lh[16] = x2n + ((hn1 + hn) >> 1);
 
@@ -424,62 +379,51 @@ rfx_rem_dwt_2d_encode_vert_lv3(const sint16 *in_buffer, sint16 *out_buffer)
     sint16 x2n2;    /* n[2n + 2] */
     sint16 hn1;     /* H[n - 1]  */
     sint16 hn;      /* H[n]      */
-    const uint16 *src;
     int n;
     int y;
-    sint16 ic[20];
+    const sint16 *ic;
+    sint16 ic14;
 
     for (y = 0; y < 17; y++)
     {
 
         /* setup */
-        src = in_buffer + y;
-        for (n = 0; n < 17; n++)
-        {
-            ic[y] = src[n * 17];
-        }
-        ic[17] = ic[15];
-        ic[18] = ic[14];
+        ic = in_buffer + y;
 
         /* pre */
         lptr = out_buffer + y;
         hptr = lptr + 17 * 9;
-        x2n = ic[0];
-        x2n1 = ic[1];
-        x2n2 = ic[2];
-        *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + hn;
+        x2n = ICL3(0);
+        x2n1 = ICL3(1);
+        x2n2 = ICL3(2);
+        HIL3(0) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+        LOL3(0) = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 7; n++)
         {
             hn1 = hn;
-            lptr += 17;
-            hptr += 17;
-            x2n = ic[2 * n];
-            x2n1 = ic[2 * n + 1];
-            x2n2 = ic[2 * n + 2];
-            *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-            *lptr = x2n + ((hn1 + hn) >> 1);
+            x2n = x2n2;
+            x2n1 = ICL3(2 * n + 1);
+            x2n2 = ICL3(2 * n + 2);
+            HIL3(n) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+            LOL3(n) = x2n + ((hn1 + hn) >> 1);
         }
 
         /* post */
         hn1 = hn;
-        lptr += 17;
-        hptr += 17;
-        x2n = ic[2 * 7];
-        x2n1 = ic[2 * 7 + 1];
-        x2n2 = ic[2 * 7 + 2];
-        *hptr = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        ic14 = x2n = x2n2;
+        x2n1 = ICL3(15);
+        x2n2 = ICL3(16);
+        HIL3(7) = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
+        LOL3(7) = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        lptr += 17;
-        x2n = ic[2 * 8];
-        x2n1 = ic[2 * 8 + 1];
-        x2n2 = ic[2 * 8 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 17 -> 15 */
+        x2n2 = ic14;      /* mirror 18 -> 14 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        *lptr = x2n + ((hn1 + hn) >> 1);
+        LOL3(8) = x2n + ((hn1 + hn) >> 1);
 
     }
 }
@@ -501,37 +445,32 @@ rfx_rem_dwt_2d_encode_horz_lv3(const sint16 *in_buffer, sint16 *out_buffer)
     const sint16 *h_src;
     int n;
     int y;
-    sint16 ic[20];
+    const sint16 *ic;
+    sint16 ic14;
 
     hl = out_buffer;
     lh = hl + 8 * 9;
     hh = lh + 9 * 8;
     ll = hh + 8 * 8;
 
-    for (y = 0; y < 9; y++)
+    for (y = 0; y < 9; y++) /* low */
     {
 
         /* setup */
-        l_src = in_buffer + y * 17;
-        for (n = 0; n < 17; n++)
-        {
-            ic[n] = l_src[n];
-        }
-        ic[17] = ic[15];
-        ic[18] = ic[14];
+        ic = in_buffer + 17 * y;
 
         /* pre */
         x2n = ic[0];
         x2n1 = ic[1];
         x2n2 = ic[2];
         hl[0] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        ll[0] = x2n + hn;
+        ll[0] = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 7; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hl[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -540,45 +479,39 @@ rfx_rem_dwt_2d_encode_horz_lv3(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 7];
-        x2n1 = ic[2 * 7 + 1];
-        x2n2 = ic[2 * 7 + 2];
+        ic14 = x2n = x2n2;
+        x2n1 = ic[15];
+        x2n2 = ic[16];
         hl[7] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         ll[7] = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        x2n = ic[2 * 8];
-        x2n1 = ic[2 * 8 + 1];
-        x2n2 = ic[2 * 8 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 17 -> 15 */
+        x2n2 = ic14;      /* mirror 18 -> 14 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         ll[8] = x2n + ((hn1 + hn) >> 1);
 
     }
 
-    for (y = 0; y < 8; y++)
+    for (y = 0; y < 8; y++) /* high */
     {
 
         /* setup */
-        h_src = l_src + 17 * 9 + y * 17;
-        for (n = 0; n < 17; n++)
-        {
-            ic[n] = h_src[n];
-        }
-        ic[17] = ic[15];
-        ic[18] = ic[14];
+        ic = in_buffer + 17 * (9 + y);
 
         /* pre */
         x2n = ic[0];
         x2n1 = ic[1];
         x2n2 = ic[2];
         hh[0] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
-        lh[0] = x2n + hn;
+        lh[0] = x2n + hn; /* mirror */
 
         /* loop */
         for (n = 1; n < 7; n++)
         {
             hn1 = hn;
-            x2n = ic[2 * n];
+            x2n = x2n2;
             x2n1 = ic[2 * n + 1];
             x2n2 = ic[2 * n + 2];
             hh[n] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
@@ -587,16 +520,16 @@ rfx_rem_dwt_2d_encode_horz_lv3(const sint16 *in_buffer, sint16 *out_buffer)
 
         /* post */
         hn1 = hn;
-        x2n = ic[2 * 7];
-        x2n1 = ic[2 * 7 + 1];
-        x2n2 = ic[2 * 7 + 2];
+        ic14 = x2n = x2n2;
+        x2n1 = ic[15];
+        x2n2 = ic[16];
         hh[7] = hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         lh[7] = x2n + ((hn1 + hn) >> 1);
 
         hn1 = hn;
-        x2n = ic[2 * 8];
-        x2n1 = ic[2 * 8 + 1];
-        x2n2 = ic[2 * 8 + 2];
+        x2n = x2n2;
+        /* x2n1 already set, mirror 17 -> 15 */
+        x2n2 = ic14;      /* mirror 18 -> 14 */
         hn = (x2n1 - ((x2n + x2n2) >> 1)) >> 1;
         lh[8] = x2n + ((hn1 + hn) >> 1);
 
